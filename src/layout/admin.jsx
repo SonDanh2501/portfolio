87a176b8f18a207994ed897ba75a37b7629eb23a
Son <sondanhwork2501@gmail.com>
2025-03-31 09:45:40 +0700

    change url link

--- src/layout/admin.jsx
+++ src/layout/admin.jsx
@@ -4,7 +4,7 @@ import LandingPage from "../pages/LandingPage";
 const Admin = () => {
     return (
       <Routes>
-        <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     );
   };
