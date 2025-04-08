d1d99ea41e3e3b6cbd2c1d9fd583561148431332
Son <sondanhwork2501@gmail.com>
2025-04-08 09:06:28 +0700

    change cv link

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -166,7 +166,7 @@ const LandingPage = () => {
           <div
             onClick={() =>
               handleClickLink(
-                "https://drive.google.com/file/d/1v55vq_0OrPvIh_PxKF7XFmGoopclNidb/view?usp=sharing"
+                "https://drive.google.com/file/d/1b0cbj0Wsi4ZkAUsKRI68xE-GoKF3k3ym/view?usp=sharing"
               )
             }
             className="landing-page__right-corner--button"
